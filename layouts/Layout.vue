4fef51b10cf884ebeead0b860e380399ec54068b
vergessenwang <469294979@qq.com>
2020-12-19 15:36:21 +0800

    packjson修改

--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -1,6 +1,14 @@
 <template>
   <!-- 如果配置了layout，那么此组件将会代替docs中的readme作为主页 -->
   <div id="home">
-    <h1>这是主页</h1>
+    <Content />
   </div>
 </template>
+<script>
+export default {
+  name: "macaroon",
+  created() {
+    console.log("是否创建layout");
+  },
+};
+</script>
