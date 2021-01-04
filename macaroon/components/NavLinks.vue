e1f28d925802206eeb6f4df286c05acdc15669f3
vergessenwang <469294979@qq.com>
2021-01-04 23:50:07 +0800

    navlinks生成多语言切换路径以及分类和标签

--- macaroon/components/NavLinks.vue
+++ macaroon/components/NavLinks.vue
@@ -40,6 +40,84 @@ export default {
         });
       });
     },
+    // 生成多语言切换路径  以及分类和标签
+    nav() {
+      const {
+        $site: { locales },
+        userNav,
+      } = this;
+      // 如果有多语言配置，展示切换路径
+      if (locales && Object.keys(locales).length > 1) {
+        const currentLink = this.$page.path;
+        const routes = this.$router.options.routes;
+        const themeLocales = this.$themeConfig.locales || {};
+        const languageDropdown = {
+          text: this.$themeLocaleConfig.selectText || "Languages",
+          items: Object.keys(locales).map((path) => {
+            const locale = locales[path];
+            const text =
+              (themeLocales[path] && themeLocales[path].label) ||
+              locale.languageDropdown;
+            let link;
+            if (locale.lang === this.$lang) {
+              link = currentLink;
+            } else {
+              link = currentLink.replace(this.$localeConfig.path, path);
+              if (!routes.some((route) => route.path === link)) {
+                link = path;
+              }
+            }
+            return { text, link };
+          }),
+        };
+        return [...userNav, languageDropdown];
+      }
+
+      // blogConfig 的处理，根据配置自动添加分类和标签
+      const blogConfig = this.$themeConfig.blogConfig || {};
+      const isHasCategory = userNav.some((item) => {
+        if (blogConfig.category) {
+          return item.text === (blogConfig.category.text || "分类");
+        } else {
+          return true;
+        }
+      });
+      const isHasTag = userNav.some((item) => {
+        if (blogConfig.tag) {
+          return item.text === (blogConfig.tag.text || "标签");
+        } else {
+          return true;
+        }
+      });
+
+      if (
+        !isHasCategory &&
+        Object.hasOwnProperty.call(blogConfig, "category")
+      ) {
+        const category = blogConfig.category;
+        const $categories = this.$categories;
+        userNav.splice(parseInt(category.location || 2) - 1, 0, {
+          items: $categories.list.map((item) => {
+            item.link = item.path;
+            item.text = item.name;
+            return item;
+          }),
+          text: category.text || "分类",
+          type: "links",
+        });
+      }
+
+      if (!isHasTag && Object.hasOwnProperty.call(blogConfig, "tag")) {
+        const tag = blogConfig.tag;
+        userNav.splice(parseInt(tag.location || 3) - 1, 0, {
+          link: "/tag/",
+          text: tag.text || "标签",
+          type: "links",
+        });
+      }
+
+      return userNav;
+    },
     // 项目仓库地址
     repoLink() {
       const { repo } = this.$site.themeConfig;
